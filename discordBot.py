c010a7cd92461932369fb5898be226aa39b19394
fianch3tt0 <cam3r0n.m0rgan@gmail.com>
2026-05-17 01:57:45 -0500

    finshed delete msg function

--- discordBot.py
+++ discordBot.py
@@ -95,7 +95,7 @@ async def userinfo(ctx):
 @bot.command()
 @commands.has_permissions(manage_messages=True)
 async def deletemsg(ctx, amount: int = 0):
-    await ctx.channel.purge(limit=amount + 1)
+    await ctx.channel.purge(limit=amount)
     await ctx.send(f"Cleared {amount} messages.", delete_after=3)
 
 bot.run(token)
